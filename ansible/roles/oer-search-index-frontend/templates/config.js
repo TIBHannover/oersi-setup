77556eace64668f5d2986544d88c0d3de6731dba
Edmond Kacaj <programim95@gmail.com>
2020-08-26 14:48:50 +0200

    I have removed the multiselct configuration

--- ansible/roles/oer-search-index-frontend/templates/config.js
+++ ansible/roles/oer-search-index-frontend/templates/config.js
@@ -6,7 +6,6 @@ window['runTimeConfig'] = {
   },
   LANGUAGE: "{{ oerindex_frontend_lang }}",
   GENERAL_CONFIGURATION: {
-    MULTI_SELECT: {{oerindex_frontend_multi_select  | lower}},  // for multiple choice set false
     RESULT_PAGE_SIZE_OPTIONS: {{oerindex_frontend_page_size}},  // page size options configuration    
     NR_OF_RESULT_PER_PAGE: {{oerindex_frontend_nr_result_page_default}},  //  number of results to show per view. Defaults to 10.   
   }
