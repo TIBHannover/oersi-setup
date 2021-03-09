5154df5b79d114c8a1eeffbbbcbc859c40ec84c8
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-03-09 07:56:44 +0100

    Added configuration of the media embedding
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/54

--- ansible/roles/oer-search-index-frontend/templates/config.js
+++ ansible/roles/oer-search-index-frontend/templates/config.js
@@ -18,6 +18,13 @@ window['runTimeConfig'] = {
     TRACK_TOTAL_HITS: {{ oerindex_frontend_track_total_hits }}, // track number of total hits from elasticsearch - see https://www.elastic.co/guide/en/elasticsearch/reference/7.10/search-your-data.html#track-total-hits
     FEATURES: {
       EMBED_OER: {{ oerindex_frontend_features_embed_oer }} // feature toggle: use "embed-oer" button
-    }
+    },
+    EMBED_MEDIA_MAPPING: [ // mappings from source url to embedding-code for media
+{% for mapping in oerindex_frontend_embed_media_mapping %}      {
+        regex: "{{ mapping.regex }}",
+        html: (match) => `{{ mapping.html }}`
+      }{{ "," if not loop.last else "" }}
+{% endfor %}
+    ]
   }
 }
