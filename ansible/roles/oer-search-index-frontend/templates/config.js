e16412d6eaa1e023a7252b5ba635377759f8bcf5
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-01-07 09:35:22 +0100

    Added frontend config for mui-header
    https://gitlab.com/oersi/oersi-frontend/-/issues/76

--- ansible/roles/oer-search-index-frontend/templates/config.js
+++ ansible/roles/oer-search-index-frontend/templates/config.js
@@ -5,6 +5,7 @@ window['runTimeConfig'] = {
     APP_NAME: "{{ elasticsearch_oer_index_internal_alias_name }}"
   },
   GENERAL_CONFIGURATION: {
+    AVAILABLE_LANGUAGES: {{ oerindex_frontend_available_languages }},
     PUBLIC_URL: "{{ oerindex_public_base_url }}{{ oerindex_public_base_path }}",
     RESULT_PAGE_SIZE_OPTIONS: {{oerindex_frontend_page_size}},  // page size options configuration    
     NR_OF_RESULT_PER_PAGE: {{oerindex_frontend_nr_result_page_default}},  //  number of results to show per view. Defaults to 10.
@@ -20,7 +21,8 @@ window['runTimeConfig'] = {
     TRACK_TOTAL_HITS: {{ oerindex_frontend_track_total_hits }}, // track number of total hits from elasticsearch - see https://www.elastic.co/guide/en/elasticsearch/reference/7.10/search-your-data.html#track-total-hits
     FEATURES: {
       EMBED_OER: {{ oerindex_frontend_features_embed_oer }}, // feature toggle: use "embed-oer" button
-      SCROLL_TOP_BUTTON: {{ oerindex_frontend_features_scroll_top_button }} // feature toggle: use "scroll-to-top" button
+      SCROLL_TOP_BUTTON: {{ oerindex_frontend_features_scroll_top_button }}, // feature toggle: use "scroll-to-top" button
+      HEADER_TYPE: "{{ oerindex_frontend_features_header_type }}" // temp feature until old reactstrap header is removed, select header-ui "mui" | "reactstrap"
     }
   }
 }
