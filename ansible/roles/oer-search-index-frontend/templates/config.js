104057b2301290350a7f96daa631eef963979a85
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-01-28 15:59:01 +0100

    Activate frontend configuration for dark mode colors
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/77

--- ansible/roles/oer-search-index-frontend/templates/config.js
+++ ansible/roles/oer-search-index-frontend/templates/config.js
@@ -11,6 +11,7 @@ window['runTimeConfig'] = {
     NR_OF_RESULT_PER_PAGE: {{oerindex_frontend_nr_result_page_default}},  //  number of results to show per view. Defaults to 10.
     HEADER_LOGO_URL: "{{ oerindex_frontend_header_logo_url }}",  // if set, use this URL for the logo in the header and include your custom svg or similar; default is "logo-192.png". You may use placeholders \{\{dark\}\} and \{\{small\}\} to use different logo-versions for dark/mobile mode (they will be replaced by "_dark" and "_small").
     THEME_COLORS: {{ oerindex_frontend_theme_colors }},  // customize colors of the theme; format: {primary: {main: "#000"}, secondary: {main: "#000"}}
+    THEME_COLORS_DARK: {{ oerindex_frontend_theme_colors_dark }},  // customize colors of the dark theme; format: {primary: {main: "#000"}, secondary: {main: "#000"}}
     /**
      * Accept a list of objects 
      * example:
