dce70d3ac6b08d02c85310e52ac5f67ca3e29cd0
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-04-11 14:35:06 +0200

    Added some ukrainian labels
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/82

--- ansible/roles/oer-search-index-backend/files/dini-ag-labels.py
+++ ansible/roles/oer-search-index-backend/files/dini-ag-labels.py
@@ -4,6 +4,11 @@ import rdflib
 # helper script that loads remote preLabels for initial, manual creation
 
 
+def get_json_from_file(file):
+    with open(file) as json_file:
+        return json.load(json_file)
+
+
 def format_labels(url):
     g = rdflib.Graph()
     g.parse(url, format="turtle")
@@ -18,10 +23,22 @@ def format_labels(url):
             labels[language] = str(l)
         output[str(s)] = labels
 
-    print(json.dumps(output, sort_keys=True, indent=2, ensure_ascii=False))
+    return output
 
 
 #format_labels('https://raw.githubusercontent.com/dini-ag-kim/hochschulfaechersystematik/master/hochschulfaechersystematik.ttl')
 #format_labels('https://raw.githubusercontent.com/acka47/lrmi-audience-role/master/educationalAudienceRole.ttl')
 #format_labels('https://raw.githubusercontent.com/dini-ag-kim/hcrt/master/hcrt.ttl')
-format_labels('https://raw.githubusercontent.com/dini-ag-kim/value-lists/main/conditionsOfAccess.ttl')
+#format_labels('https://raw.githubusercontent.com/dini-ag-kim/value-lists/main/conditionsOfAccess.ttl')
+json_labels = get_json_from_file('labels-hochschulfaechersystematik.json')
+data = format_labels('https://raw.githubusercontent.com/dini-ag-kim/hochschulfaechersystematik/21-ukrainische-label-hinzuf%C3%BCgen/hochschulfaechersystematik.ttl')
+
+for key in json_labels:
+    if key not in data:
+        data[key] = json_labels[key]
+    else:
+        for lng in json_labels[key]:
+            if lng not in data[key]:
+                data[key][lng] = json_labels[key][lng]
+
+print(json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False))
