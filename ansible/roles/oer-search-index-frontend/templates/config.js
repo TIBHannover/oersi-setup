0e2720a732a321863170ad04a0952fae0e266504
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-06-30 07:47:43 +0200

    Removed comment from template to reduce network-traffic
    still available in the frontend
    
    Ref https://gitlab.com/oersi/oersi-setup/-/issues/34

--- ansible/roles/oer-search-index-frontend/templates/config.js
+++ ansible/roles/oer-search-index-frontend/templates/config.js
@@ -4,30 +4,24 @@ window['runTimeConfig'] = {
   GENERAL_CONFIGURATION: {
     AVAILABLE_LANGUAGES: {{ oerindex_frontend_available_languages }},
     PUBLIC_URL: "{{ oerindex_public_base_url }}{{ oerindex_public_base_path }}",
-    RESULT_PAGE_SIZE_OPTIONS: {{oerindex_frontend_page_size}},  // page size options configuration    
-    NR_OF_RESULT_PER_PAGE: {{oerindex_frontend_nr_result_page_default}},  //  number of results to show per view. Defaults to 10.
-    HEADER_LOGO_URL: "{{ oerindex_frontend_header_logo_url }}",  // if set, use this URL for the logo in the header and include your custom svg or similar; default is "logo-192.png". You may use placeholders \{\{dark\}\} and \{\{small\}\} to use different logo-versions for dark/mobile mode (they will be replaced by "_dark" and "_small").
-    THEME_COLORS: {{ oerindex_frontend_theme_colors }},  // customize colors of the theme; format: {primary: {main: "#000"}, secondary: {main: "#000"}}
-    THEME_COLORS_DARK: {{ oerindex_frontend_theme_colors_dark }},  // customize colors of the dark theme; format: {primary: {main: "#000"}, secondary: {main: "#000"}}
-    /**
-     * Accept a list of objects 
-     * example:
-     * {'path': 'public/{folderName}/{languageCode}/{fileName}.html', 'language': '{languageCode}'}
-     * 
-     */
+    RESULT_PAGE_SIZE_OPTIONS: {{oerindex_frontend_page_size}},
+    NR_OF_RESULT_PER_PAGE: {{oerindex_frontend_nr_result_page_default}},
+    HEADER_LOGO_URL: "{{ oerindex_frontend_header_logo_url }}",
+    THEME_COLORS: {{ oerindex_frontend_theme_colors }},
+    THEME_COLORS_DARK: {{ oerindex_frontend_theme_colors_dark }},
     PRIVACY_POLICY_LINK: {{oerindex_frontend_custom_cookie_links | default([], true) | to_json(ensure_ascii=False,indent=0) }},
-    I18N_CACHE_EXPIRATION: {{ oerindex_frontend_i18n_cache_expiration }}, // expiration time of the i18n translation cache storage
+    I18N_CACHE_EXPIRATION: {{ oerindex_frontend_i18n_cache_expiration }},
     I18N_DEBUG: {{ oerindex_frontend_i18n_debug }},
-    TRACK_TOTAL_HITS: {{ oerindex_frontend_track_total_hits }}, // track number of total hits from elasticsearch - see https://www.elastic.co/guide/en/elasticsearch/reference/7.10/search-your-data.html#track-total-hits
-    AGGREGATION_SEARCH_COMPONENTS: {{ oerindex_frontend_aggregation_search_components }}, // filters/components that should update the aggregation on typing in the search field
-    AGGREGATION_SEARCH_DEBOUNCE: {{ oerindex_frontend_aggregation_search_debounce }}, // sets the milliseconds to wait before executing an aggregation search (search inside filters)
-    AGGREGATION_SEARCH_MIN_LENGTH: {{ oerindex_frontend_aggregation_search_min_length }}, // minimum length of search term in aggregation search (search inside filters)
+    TRACK_TOTAL_HITS: {{ oerindex_frontend_track_total_hits }},
+    AGGREGATION_SEARCH_COMPONENTS: {{ oerindex_frontend_aggregation_search_components }},
+    AGGREGATION_SEARCH_DEBOUNCE: {{ oerindex_frontend_aggregation_search_debounce }},
+    AGGREGATION_SEARCH_MIN_LENGTH: {{ oerindex_frontend_aggregation_search_min_length }},
     FEATURES: {
       DARK_MODE: {{ oerindex_frontend_features_dark_mode }},
-      CHANGE_FONTSIZE: {{ oerindex_frontend_features_change_font_size }},  // experimental/beta - just to show weaknesses in styling
-      EMBED_OER: {{ oerindex_frontend_features_embed_oer }}, // feature toggle: use "embed-oer" button
-      OERSI_THUMBNAILS: {{ oerindex_frontend_features_oersi_thumbnails }}, // feature toggle: use thumbnails from OERSI-thumbnail-generator for resource-preview-images with image-url as fallback
-      SCROLL_TOP_BUTTON: {{ oerindex_frontend_features_scroll_top_button }} // feature toggle: use "scroll-to-top" button
+      CHANGE_FONTSIZE: {{ oerindex_frontend_features_change_font_size }},
+      EMBED_OER: {{ oerindex_frontend_features_embed_oer }},
+      OERSI_THUMBNAILS: {{ oerindex_frontend_features_oersi_thumbnails }},
+      SCROLL_TOP_BUTTON: {{ oerindex_frontend_features_scroll_top_button }}
     }
   }
 }
